713fb98f20c9c7cef71df69055829ba1f1f0ce40
siddhu9-lost <dbcface@gmail.com>
2026-06-09 02:11:03 +0530

    server.js

--- ewaste-project/server.js
+++ ewaste-project/server.js
@@ -133,3 +133,17 @@ function showNotification(msg, type) {
     document.body.appendChild(toast);
     setTimeout(() => toast.remove(), 3000);
 }
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`EcoCycle Server running securely on http://localhost:${PORT}`);
+});
